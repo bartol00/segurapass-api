0e80d6e9de6f41cb1ea9cc2ec431be8add472f70
Bartol <bartolx555@gmail.com>
2026-05-09 16:25:32 +0200

    Adjusted RefreshReq

--- src/main/java/xyz/segurapass/api/authorization/RefreshReq.java
+++ src/main/java/xyz/segurapass/api/authorization/RefreshReq.java
@@ -4,13 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.UUID;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class RefreshReq {
-    private String email;
-    private UUID deviceId;
     private String refreshToken;
 }
