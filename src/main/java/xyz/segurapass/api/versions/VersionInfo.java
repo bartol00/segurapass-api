f887086c2812f05bda097894e10eff5f0d7375d7
Damjan Bartol Benjak <95582435+bartol00@users.noreply.github.com>
2026-07-29 19:54:59 +0200

    VersionInfo change (#9)

--- src/main/java/xyz/segurapass/api/versions/VersionInfo.java
+++ src/main/java/xyz/segurapass/api/versions/VersionInfo.java
@@ -5,18 +5,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class VersionInfo {
-    @JsonProperty("versionNumber")
-    private String versionNumber;
-    @JsonProperty("versionDescription")
-    private String versionDescription;
-    @JsonProperty("downloadUrl")
-    private String downloadUrl;
-    @JsonProperty("versionDate")
-    private LocalDate versionDate;
+    @JsonProperty("appVersion")
+    private String appVersion;
+    @JsonProperty("protocolVersion")
+    private String protocolVersion;
 }
