90d1862925684833737eff0c474c493ce3645959
Bartol <bartolx555@gmail.com>
2026-08-10 21:31:20 +0200

    additional totp objects

---
+++ src/main/java/xyz/segurapass/api/mfa/TotpRecoveryReq.java
@@ -0,0 +1,14 @@
+package xyz.segurapass.api.mfa;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TotpRecoveryReq {
+    @JsonProperty("recoveryCode")
+    private String recoveryCode;
+}
